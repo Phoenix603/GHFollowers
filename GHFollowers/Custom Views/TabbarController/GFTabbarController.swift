0ab6eca6aab22b5f6a2ed2dc70f90a46c8eb1f16
Aleksei Zakharov <2zakharov.ag@gmail.com>
2020-03-11 20:29:38 -0700

    Complete project

---
+++ GHFollowers/Custom Views/TabbarController/GFTabbarController.swift
@@ -0,0 +1,35 @@
+//
+//  GFTabbarController.swift
+//  GHFollowers
+//
+//  Created by Aleksei Zakharov on 3/11/20.
+//  Copyright © 2020 Aleksei Zakharov. All rights reserved.
+//
+
+import UIKit
+
+class GFTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers                 = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC  = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem.init(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+           let favoritesListVC  = FavoritesListVC()
+           favoritesListVC.title = "Favorites"
+           favoritesListVC.tabBarItem = UITabBarItem.init(tabBarSystemItem: .favorites, tag: 1)
+           
+           return UINavigationController(rootViewController: favoritesListVC)
+    }
+
+}
